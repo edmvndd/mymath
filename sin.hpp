77df3ee159d010d12386effb6a74a561365d3a37
Edmund Anglero <edmund.jr.anglero@gmail.com>
2020-08-03 18:24:22 -0400

    iterative factorial function

--- sin.hpp
+++ sin.hpp
@@ -8,8 +8,12 @@ double sin(double &x)
 
 int factorial(int &n)
 {
-    for(int i = n; i > 0; i--)
+    int fact = 1;
+
+    for(int i = n; i > 1; i--)
     {
-        
+        fact *= i;
     }
+
+    return fact;
 }
